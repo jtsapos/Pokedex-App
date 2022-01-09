fe69ab40b2c6f62b9758b46f8f697b4d6aeb95b5
jtsapos <jtsapos@gmx.com>
2022-01-09 15:58:03 -0500

    reformatted code for consistency

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,2 @@
-let pokemonList=[{name: "Bulbasaur", height: 7, type: ['grass', 'poison'] }, {name: "Charizard", height: 1.7, type: ['fire', 'flying'] }, {name: "Butterfree", height: 1, type: ['bug', 'flying']}];
+let pokemonList=[{name: "Bulbasaur", height: 7, type: ['grass', 'poison'] }, {name: "Charizard", height: 1.7, type:
+['fire', 'flying'] }, {name: "Butterfree", height: 1, type: ['bug', 'flying']}];
