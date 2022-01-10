2965db19fc50d561edb58dc4819359eca76a2aa5
jtsapos <jtsapos@gmx.com>
2022-01-10 16:39:19 -0500

    Updated format in array

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,3 @@
-let pokemonList=[{name: "Bulbasaur", height: 7, type: ['grass', 'poison'] }, {name: "Charizard", height: 1.7, type:
-['fire', 'flying'] }, {name: "Butterfree", height: 1, type: ['bug', 'flying']}];
+let pokemonList=[{name: "Bulbasaur", height: 7, type: ['grass', 'poison'] },
+                 {name: "Charizard", height: 1.7, type: ['fire', 'flying'] },
+                 {name: "Butterfree", height: 1, type: ['bug', 'flying']}];
