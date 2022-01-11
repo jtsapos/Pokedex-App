50603a6b50c17477c5a0415ca1a270145032e8f7
jtsapos <jtsapos@gmx.com>
2022-01-11 15:19:44 -0500

    added a comma between name and height

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,7 @@ let pokemonList=[{name: "Bulbasaur", height: .7, type: ['grass', 'poison'] },
                  // Initialization "let i = 0", the condition "i < pokemonList.length;"
                  // and the action "i++ which is the equivalent of i = i + 1"
                  for (let i = 0; i < pokemonList.length; i++) {
-                   document.write('<br>' + pokemonList[i].name + (' height: ') + pokemonList[i].height);
+                   document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
                     //This handles heights greater than or equal to 1.7
                    if (pokemonList[i].height >= 1.7) {
                      document.write(' - Wow, that\'s a big Pokemon!');
