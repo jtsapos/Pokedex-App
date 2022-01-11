d304d660a22c4fffd8f419c1adf925476f7a4984
jtsapos <jtsapos@gmx.com>
2022-01-11 13:36:48 -0500

    Begin for loop: inititalization, condition, action

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
-let pokemonList=[{name: "Bulbasaur", height: 7, type: ['grass', 'poison'] },
+let pokemonList=[{name: "Bulbasaur", height: .7, type: ['grass', 'poison'] },
                  {name: "Charizard", height: 1.7, type: ['fire', 'flying'] },
                  {name: "Butterfree", height: 1, type: ['bug', 'flying']}];
+
+                 // Initialization "let i = 0", the condition "i < pokemonList.length;"
+                 // and the action "i++ which is the equivalent of i = i + 1"
+                 for (let i = 0; i < pokemonList.length; i++) {
+                    
