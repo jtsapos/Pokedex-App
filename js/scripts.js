ded94a68206f48c9b05cc0c26e417a875c765088
jtsapos <jtsapos@gmx.com>
2022-01-11 13:39:32 -0500

    Document.write to display the Pokemon name on DOM

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,4 @@ let pokemonList=[{name: "Bulbasaur", height: .7, type: ['grass', 'poison'] },
                  // Initialization "let i = 0", the condition "i < pokemonList.length;"
                  // and the action "i++ which is the equivalent of i = i + 1"
                  for (let i = 0; i < pokemonList.length; i++) {
-                    
+                   document.write(pokemonList[i].name + (' , height: ') + pokemonList[i].height);
