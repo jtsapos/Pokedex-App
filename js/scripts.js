ddd7184c7a03b3dd9dd215bcfb134cc42031d306
jtsapos <jtsapos@gmx.com>
2022-01-17 15:05:03 -0500

    1)Refactored code to use forEach() function. 2)Pokemon list wrapped in an IIFE 3)Added getall and add functions to repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,57 @@
-let pokemonList = [{
-    name: "Bulbasaur",
-    height: .7,
-    type: ['grass', 'poison']
-  },
-  {
-    name: "Charizard",
-    height: 1.7,
-    type: ['fire', 'flying']
-  },
-  {
-    name: "Butterfree",
-    height: 1,
-    type: ['bug', 'flying']
-  },
-  {
-    name: "Weedle",
-    height: .3,
-    type: ['bug', 'poison']
-  },
-  {
-    name: "Fearow",
-    height: 1.2,
-    type: ['flying', 'normal']
-  },
-  {
-    name: "Nidoking",
-    height: 1.4,
-    type: ['ground', 'poison']
-  }
-];
+let pokemonRepository = (function() {
 
-function pokemonLoop(user) {
+  let pokemonList = [
+
+    {
+      name: "Bulbasaur",
+      height: .7,
+      type: ['grass', 'poison']
+    },
+    {
+      name: "Charizard",
+      height: 1.7,
+      type: ['fire', 'flying']
+    },
+    {
+      name: "Butterfree",
+      height: 1,
+      type: ['bug', 'flying']
+    },
+    {
+      name: "Weedle",
+      height: .3,
+      type: ['bug', 'poison']
+    },
+    {
+      name: "Fearow",
+      height: 1.2,
+      type: ['flying', 'normal']
+    },
+    {
+      name: "Nidoking",
+      height: 1.4,
+      type: ['ground', 'poison']
+    }
+  ]
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+
+})();
+
+function pokemonLoop(item) {
+  document.write(pokemonRepository.getAll());
   document.write('<br>' + user.name + ' height is ' + user.height + ' and attacks used are ' + user.type);
+
 }
 pokemonList.forEach(pokemonLoop);
