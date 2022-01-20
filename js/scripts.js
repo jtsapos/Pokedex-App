d80f81a506cae54a561b22bf52a81a5f6cc916c9
jtsapos <jtsapos@gmx.com>
2022-01-20 14:08:04 -0500

    added button-class to styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonRepository = (function() {
 
-  let pokemonList = [
+  let repository = [
 
     {
       name: "Bulbasaur",
@@ -31,34 +31,35 @@ let pokemonRepository = (function() {
       name: "Nidoking",
       height: 1.4,
       type: ['ground', 'poison']
-    }
-  ]
+    },
+  ];
 
   function getAll() {
-    return pokemonList;
+    return repository;
   }
 
-  function add(item) {
-    pokemonList.push(item);
+  function addListItem(pokemon) {
+    let pokemonList = document.queryselector(".pokemon-list"); //create new varible pokemonList wch takes queryselector wch is .pokemon-list class(frm index.html)
+    let listpokemon = document.createElement("li"); //after creating ul element we create an li element using createElement
+    let button = document.createElement("button"); //inside each li we create a button tag with createElement
+    button.innerText = item.name; //renders the button, adds pokemon name inside the button
+    button.classlist.add("button-class"); //add css class to style the button from styles.css
+    listpokemon.appendChild(button); //calls the listpokemon and appends the child button to the li (ea button is an li)
+    pokemonList.appendChild(listpokemon); //now we append the li to the ul (.pokemon-List)(which is the main element or parent element)
+  }
+
+  function add(pokemon) {
+    repository.push(pokemon);
   }
 
   return {
     add: add,
     getAll: getAll,
+    addListItem: addListItem
   };
 
 })();
-//console.log(pokemonRepository.getAll());
-let pokemonList = document.queryselector(".pokemon-list"); //create new varible pokemonList wch takes queryselector wch is .pokemon-list class(frm index.html)
-let listpokemon = document.createElement("li"); //after creating ul element we create an li element using createElement
-let button = document.createElement("button"); //inside each li we create a button tag with createElement
-button.innerText = pokemon.name; //renders the button, adds pokemon name inside the button
-button.classlist.add("button-class"); //add css class to style the button from styles.css
-listpokemon.appendChild(button); //calls the listpokemon and appends the child button to the li (ea button is an li)
-pokemonList.appendChild(listpokemon); //now we append the li to the ul (.pokemon-List)(which is the main element or parent element)
-
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write("<br>" + item.name + " : " + item.height + " : " + item.type);
-  if (item.height >= 1.7)
-    document.write(' - Wow, that\'s a big Pokemon!');
+  pokemonRepository.addListItem(pokemon);
+
 });
