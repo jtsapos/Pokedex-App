ea3357815a91229fcc5540396d6f44a6ec64def7
jtsapos <jtsapos@gmx.com>
2022-01-20 15:46:43 -0500

    function add(pokemon) added to be able to add additional pokemon object

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,19 @@ let pokemonRepository = (function() {
     },
   ];
 
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      document.write("pokemon is not correct");
+    }
+  }
+
   function getAll() {
     return repository;
   }
@@ -42,14 +55,10 @@ let pokemonRepository = (function() {
     let pokemonList = document.queryselector(".pokemon-list"); //create new varible pokemonList wch takes queryselector wch is .pokemon-list class(frm index.html)
     let listpokemon = document.createElement("li"); //after creating ul element we create an li element using createElement
     let button = document.createElement("button"); //inside each li we create a button tag with createElement
-    button.innerText = item.name; //renders the button, adds pokemon name inside the button
+    button.innerText = pokemon.name; //renders the button, adds pokemon name inside the button
     button.classlist.add("button-class"); //add css class to style the button from styles.css
     listpokemon.appendChild(button); //calls the listpokemon and appends the child button to the li (ea button is an li)
     pokemonList.appendChild(listpokemon); //now we append the li to the ul (.pokemon-List)(which is the main element or parent element)
-  }
-
-  function add(pokemon) {
-    repository.push(pokemon);
   }
 
   return {
@@ -59,6 +68,8 @@ let pokemonRepository = (function() {
   };
 
 })();
+
+//console.log(pokemonRepository.getAll());
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
 
