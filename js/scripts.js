1a2fee24edf30da831184d4fc843758d7f6c92a8
jtsapos <jtsapos@gmx.com>
2022-02-17 14:34:17 -0500

    Line 102 'modalHeader' is assigned a value but never used

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ let pokemonRepository = (function() { //creates an IIFE for pokemon list
       let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=76';
       let modalContainer = document.querySelector('#exampleModal'); //acts as a global variable where all the functions below have access to it.
       function add(pokemon) { // function to add a pokemon to the pokemonList
-        if (typeof pokemon === "object" && "name" in pokemon) { //&& //if pokemon is an object and has a name we will push the pokemon in the repository
+        if (typeof pokemon === 'object' && 'name' in pokemon) { //&& //if pokemon is an object and has a name we will push the pokemon in the repository
           //"detailsUrl" in pokemon
           pokemonList.push(pokemon);
         } else {
-          document.write("Pokemon is not correct");
+          document.write('Pokemon is not correct');
         }
       }
 
@@ -17,25 +17,25 @@ let pokemonRepository = (function() { //creates an IIFE for pokemon list
 
       function addListItem(pokemon) {
         loadDetails(pokemon).then(function() {
-        let pokemonList = document.querySelector(".pokemon-list"); //create new varible pokemonList wch takes queryselector wch is .pokemon-list class(frm index.html)
-        let listpokemon = document.createElement("li"); //after creating ul element we create an li element using createElement
-        let button = document.createElement("button"); //inside each li we create a button tag with createElement
-        let pokemonImage = document.createElement("img"); //here we create an image tag
-        let pokemonName = document.createElement("div"); //here we create a name tag
+        let pokemonList = document.querySelector('.pokemon-list'); //create new varible pokemonList wch takes queryselector wch is .pokemon-list class(frm index.html)
+        let listpokemon = document.createElement('li'); //after creating ul element we create an li element using createElement
+        let button = document.createElement('button'); //inside each li we create a button tag with createElement
+        let pokemonImage = document.createElement('img'); //here we create an image tag
+        let pokemonName = document.createElement('div'); //here we create a name tag
         pokemonImage.src = pokemon.imageUrlFront;
-        pokemonImage.alt= "image " + pokemon.name;
+        pokemonImage.alt= 'image ' + pokemon.name;
         pokemonName.innerText = pokemon.name; 
-        button.innerText = "See details"; //renders the button, adds pokemon name inside the button
-        listpokemon.classList.add("list-group-item");
-        button.classList.add("btn", "btn-primary");//add bootstrap css class to style your list of elements with some borders and padding
+        button.innerText = 'See details'; //renders the button, adds pokemon name inside the button
+        listpokemon.classList.add('list-group-item');
+        button.classList.add('btn', 'btn-primary');//add bootstrap css class to style your list of elements with some borders and padding
         listpokemon.appendChild(pokemonName); //calls the listpokemon and appends the Pokemon name
         listpokemon.appendChild(pokemonImage); //calls the listpokemon and appends the Pokemon image
         listpokemon.appendChild(button); //calls the listpokemon and appends the child button to the li (ea button is an li)
         pokemonList.appendChild(listpokemon); //now we append the li to the ul (.pokemon-List)(which is the main element or parent element)
-        button.setAttribute("data-target", "#exampleModal");
-        button.setAttribute("data-toggle", "modal");
+        button.setAttribute('data-target', '#exampleModal');
+        button.setAttribute('data-toggle', 'modal');
         //function buttonEventListener(button, pokemon) { // When the user clicks the selected button, the click function passed as the second parameter in addEventListener will be called to show pokemon details.
-        button.addEventListener("click", function(event) { //event listener which listens for a mouseclick, then executes the show details function for the pokemon
+        button.addEventListener('click', function(event) { //event listener which listens for a mouseclick, then executes the show details function for the pokemon
             showDetails(pokemon);
             // function that prints pokemon details onto console
             });
@@ -97,31 +97,31 @@ let pokemonRepository = (function() { //creates an IIFE for pokemon list
 
           //show modal content
           function showModal(pokemon) {
-            let modalBody = $(".modal-body");
-            let modalTitle = $(".modal-title");
-            let modalHeader = $(".modal-header");
+            let modalBody = $('.modal-body');
+            let modalTitle = $('.modal-title');
+            //let modalHeader = $('.modal-header');
             // let $modalContainer = $("#modal-container");
             //clear existing content of the modal
             // modalHeader.empty();
             modalTitle.empty();
             modalBody.empty();
             //creating element for name in modal content
-            let nameElement = $("<h1>" + pokemon.name + "</h1>");
+            let nameElement = $('<h1>' + pokemon.name + '</h1>');
             // // creating element for image in modal content
             let imageElementFront = $('<img class="modal-img" style="width:50%">');
-            imageElementFront.attr("alt", "image " + pokemon.name);
-            imageElementFront.attr("src", pokemon.imageUrlFront);
+            imageElementFront.attr('alt', 'image ' + pokemon.name);
+            imageElementFront.attr('src', pokemon.imageUrlFront);
             let imageElementBack = $('<img class="modal-img" style="width:50%">');
-            imageElementBack.attr("alt", "image " + pokemon.name);
-            imageElementBack.attr("src", pokemon.imageUrlBack);
+            imageElementBack.attr('alt', 'image ' + pokemon.name);
+            imageElementBack.attr('src', pokemon.imageUrlBack);
             // //creating element for height in modal content
-            let heightElement = $("<p>" + "height : " + pokemon.height + "</p>");
+            let heightElement = $('<p>' + 'height : ' + pokemon.height + '</p>');
             // //creating element for weight in modal content
-            let weightElement = $("<p>" + "weight : " + pokemon.weight + "</p>");
+            let weightElement = $('<p>' + 'weight : ' + pokemon.weight + '</p>');
             // //creating element for type in modal content
-            let typesElement = $("<p>" + "types : " + pokemon.types + "</p>");
+            let typesElement = $('<p>' + 'types : ' + pokemon.types + '</p>');
             // //creating element for abilities in modal content
-            let abilitiesElement = $("<p>" + "abilities : " + pokemon.abilities + "</p>");
+            let abilitiesElement = $('<p>' + 'abilities : ' + pokemon.abilities + '</p>');
 
             ////Now we need to append these elements to the modal
             modalTitle.append(nameElement);
